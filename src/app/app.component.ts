025a1c999882917fca80c09a2c1b233ed6245f9a
cnk224 <oumartoure405@gmail.com>
2021-05-10 10:49:27 +0200

    Utilisation d'un boucle pour afficher les heures de biberons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'code911ProjetctView';
-  name1 = 'Alexia';
-  name2 = 'Oumar';
-  dateHeureBibTs = 'Le 10/05/2021 à 12:00';
-  quantiteTs = '20 cl';
+  bibInfos = [
+    {
+      name: 'Alexia',
+      dateHeureBibTs : 'Le 10/05/2021 à 12:00',
+      quantiteTs: '20 cl'
+    },
+    {
+      name: 'Oumar',
+      dateHeureBibTs : 'Le 10/05/2021 à 15:00',
+      quantiteTs: '23 cl'
+    },
+    {
+      name: 'Alexia',
+      dateHeureBibTs : 'Le 10/05/2021 à 18:00',
+      quantiteTs: '21 cl'
+    }
+  ]
 }
